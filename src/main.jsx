fd38013a1c04a8844762ba576a5d91b061cdd1b0
moksh555 <57554299+moksh555@users.noreply.github.com>
2024-09-27 13:10:22 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import React, { StrictMode } from "react";
-import ReactDom, { createRoot } from "react-dom/client";
+import ReactDom, { createRoot } from "react-dom";
 import App from "./App.jsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+// createRoot(document.getElementById("root")).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>
+// );
 
+
+ReactDom.render(<App/>, document.getElementbyId("root"))
 
